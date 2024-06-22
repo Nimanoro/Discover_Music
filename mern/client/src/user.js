bd36a1c4f841829d90639eea3121b32b4a9dfb6b
Nimanoro <nimanourozy@gmail.com>
2024-06-22 15:32:01 -0700

    Refactor User.js to remove unused state variables and fix formatting

--- mern/client/src/user.js
+++ mern/client/src/user.js
@@ -1,12 +1,5 @@
 
 const User = () => {
-    const [userProfile, setUserProfile] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [sortOption, setSortOption] = useState('newest');
-    const playlistsPerPage = 16;
-    const [showPopup, setShowPopup] = useState(false); 
     const API_URL = process.env.REACT_APP_API_URI;
     
     return (< div> Nima is a bit du
