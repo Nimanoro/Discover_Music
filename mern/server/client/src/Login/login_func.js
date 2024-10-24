27dbed504e9a5e8b168f731891dd9dff22586978
NimaNoro <nimanourozy@gmail.com>
2024-10-24 16:23:36 -0700

    show_dialog parameter to authorization URI

--- mern/server/client/src/Login/login_func.js
+++ mern/server/client/src/Login/login_func.js
@@ -4,7 +4,7 @@ const loginWithSpotify = () => {
     const scope = process.env.REACT_APP_SCOPE;
     const clientId = process.env.REACT_APP_CLIENTID;
     const redirectUri = process.env.REACT_APP_REDIRECTURI;
-    const uri = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
+    const uri = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&show_dialog=true`;
     return uri;
   };
 
