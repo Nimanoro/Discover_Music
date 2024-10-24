3b3cfeb6246f8d6fa887889a0d1c71e05b9ecc4e
NimaNoro <nimanourozy@gmail.com>
2024-10-24 16:34:21 -0700

    Refactor playlist item rendering in User component

--- mern/server/client/src/Login/user.js
+++ mern/server/client/src/Login/user.js
@@ -71,14 +71,14 @@ const User = () => {
       <div className="playlist-grid mb-5">
         {currentPlaylists.map(playlist => (
           <div key={playlist.id} className="playlist-item">
-            <div className="playlist-image-container">
+            <div className="playlist-image-container playlist-buttons">
               {playlist.image ? (
                 <img src={playlist.image} alt={playlist.name} className="playlist-image" />
               ) : (
                 <img src="default_image_url" alt="Default" className="playlist-image" />
               )}
             </div>
-            <div className="playlist-buttons">
+            <div>
               <button 
                 className="btn-play"
                 type="button"
